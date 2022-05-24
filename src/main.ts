6a8196d78f33dfd4b9bbebd47da9f7c269ace534
herasymenko <herasymenko@zfort.com>
2022-05-24 16:18:13 +0300

    refactor: swagger tag

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const bootstrap = async () => {
     const app = await NestFactory.create(AppModule, { cors: true });
-    const config = new DocumentBuilder().setTitle('My Unsplash API').setDescription('The My Unsplash API').setVersion('1.0').addTag('unsplash').build();
+    const config = new DocumentBuilder().setTitle('My Unsplash API').setDescription('The My Unsplash API').setVersion('1.0').build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('/api/v1', app, document);
     await app.listen(AppModule.port);
